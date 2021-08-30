e1321822d13e813dd5768dd22e130182bf0f0b0a
AndreyKryukov <ander.kryukov@yandex.ru>
2021-08-30 23:07:29 +0300

    feat: solved the problem

--- src/index.js
+++ src/index.js
@@ -4,7 +4,18 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    let openParenthesis = 0,
+        closingParenthesis = 0;
+
+    for (let i = 0; i < expr.length; i++) {
+        if (expr[i] === "(") openParenthesis++;
+        if (expr[i] === ")") closingParenthesis++;
+    }
+
+    if (openParenthesis !== closingParenthesis) throw new Error("ExpressionError: Brackets must be paired.");
+    if (expr.indexOf("/ 0") + 1) throw new Error("TypeError: Division by zero.");
+
+    return new Function(`return ${expr}`)();
 }
 
 module.exports = {
